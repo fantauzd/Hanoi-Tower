1fcd7d7ba2d0ef38adf678c23a987910e318527c
Dominic Fantauzzo <137939470+fantauzd@users.noreply.github.com>
2024-04-13 16:51:45 -0400

    Add files via upload
    initial commit. Problem solved

---
+++ Hanoi.py
@@ -0,0 +1,27 @@
+def hanoi(n, source, temp, target):
+    """
+    Solution to the Tower of Hanoi Problem.
+    Moves n discs from the first tower to the target tower.
+    :param n: number of discs to be moved
+    :param source: Array containing discs in starting position.
+    :param temp: An empty array
+    :param target: An empty array.
+    :return: None
+    """
+    if n > 0:
+        # move n-1 disks from source to temp:
+        hanoi(n - 1, source, target, temp)
+        # move disk from source peg to target peg
+        if source:
+            target.append(source.pop())
+        # move n-1 disks from temp to target
+        hanoi(n - 1, temp, source, target)
+
+
+if __name__ == "__main__":
+    source = [5, 4, 3, 2, 1]
+    target = []
+    temp = []
+    hanoi(len(source), source, temp, target)
+
+    print(source, temp, target)
